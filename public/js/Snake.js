a51bd8392980e72d332d8672661aec66095ae831
Kieran <kieran.milligan@gmail.com>
2020-02-28 22:53:01 -0600

    finished for night, correct background url

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -3,7 +3,7 @@ export default class Snake {
     this.screen = screen;
     this.lastMoveTime = 0;
     this.moveInterval = 400;
-    this.tileSize = 16;
+    this.tileSize = 8;
     this.direction = Phaser.Math.Vector2.RIGHT;
     this.body = [];
 
@@ -18,11 +18,11 @@ export default class Snake {
         )
         .setOrigin(0)
     );
-    // added second square in green
+    // added second square in green for VHC cosmetics
     this.body.push(
       this.screen.add
         .rectangle(
-          this.screen.game.config.width / 2, this.screen.game.config.height / 2,
+          (this.screen.game.config.width / 2), (this.screen.game.config.height / 2),
           this.tileSize,
           this.tileSize,
           0x66ff00
@@ -30,32 +30,31 @@ export default class Snake {
         .setOrigin(0)
     );
 
-    this.apple = this.screen.add
+    this.fruit = this.screen.add
       .rectangle(0,0,this.tileSize,this.tileSize,0x6a0dad)
       .setOrigin(0);
 
-    this.pickApples();
+    this.pickFruits();
 
     screen.input.keyboard.on('keydown', e => {
       this.keydown(e);
     });
   }
 
-  pickApples() {
-    this.apple.x = Math.floor((Math.random() * this.screen.game.config.width) / this.tileSize) * this.tileSize;
-    this.apple.y = Math.floor((Math.random() * this.screen.game.config.height) / this.tileSize) * this.tileSize;
+  pickFruits() {
+    this.fruit.x = Math.floor((Math.random() * this.screen.game.config.width) / this.tileSize) * this.tileSize;
+    this.fruit.y = Math.floor((Math.random() * this.screen.game.config.height) / this.tileSize) * this.tileSize;
   }
 
+  // movement controls; `if` statement prevents going opposite direction/Game Over
   keydown(event) {
     console.log(event);
     switch(event.keyCode) {
-
       case 37:
         if(this.direction != Phaser.Math.Vector2.RIGHT) {
           this.direction = Phaser.Math.Vector2.LEFT;
         }
         break;
-
       case 38:
         if (this.direction != Phaser.Math.Vector2.DOWN) {
           this.direction = Phaser.Math.Vector2.UP;
@@ -81,37 +80,39 @@ export default class Snake {
     }
   }
 
+  // let startBtn = document.getElementById('startBtn');
+
   move() {
     // where head (`body[0]`) is moving to, saved as variables `x` and `y`
     let x = this.body[0].x + this.direction.x * this.tileSize;
     let y = this.body[0].y + this.direction.y * this.tileSize;
 
-    // if apple and head are in same tile, eat apple
-    if (this.apple.x === x && this.apple.y === y) {
+    // if fruit and head are in same tile, eat fruit
+    if (this.fruit.x === x && this.fruit.y === y) {
       // push new rectangle to body array
       this.body.push(
         this.screen.add
         .rectangle(0,0,this.tileSize,this.tileSize,0x66ff00)
         .setOrigin(0)
       );
-      // remove eaten apple
-      this.pickApples();
+      // remove eaten fruit
+      this.pickFruits();
       // for every 3 eaten, decrease moveInterval
       if (this.body.length % 3 === 0) {
         this.moveInterval -= 50;
       }
     }
 
-
-
     // render `body.length`
     for (let index = this.body.length - 1; index > 0; index--) {
       this.body[index].x = this.body[index - 1].x;
       this.body[index].y = this.body[index - 1].y;
     }
+
     // direction `head` is going
     this.body[0].x = x;
     this.body[0].y = y;
+
 
     // snake dies
     // if snake goes off board...
@@ -119,13 +120,13 @@ export default class Snake {
       // snake dies, game restarts
       this.screen.scene.restart();
     }
+
     // dies running over tail
     let tail = this.body.slice(1);
     // if `head` goes over any `tailSquare`...
     if (tail.filter(tailSquare => tailSquare.x === this.body[0].x && tailSquare.y === this.body[0].y).length > 0) {
       // snake dies, game restarts
       this.screen.scene.restart();
-
     }
   }
 }
--- public/js/snakePit.js
+++ public/js/snakePit.js
@@ -1,8 +1,8 @@
 import MainScene from "./MainScreen.js";
 // create gamespace
 const config = {
-  width: 480,
-  height: 480,
+  width: 240,
+  height: 240,
   type: Phaser.AUTO,
   parent: "snake",
   scene: [MainScene]
